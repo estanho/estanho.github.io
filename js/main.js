86dc5f4541ed0c38dda6eff86e1997dafb1b5c47
Pedro Henrique da Rosa Garcia <estanhoeu@gmail.com>
2022-07-11 20:59:23 -0300

    Muitas coisas 😎🌹

--- js/main.js
+++ js/main.js
@@ -6,6 +6,16 @@ $(document).ready(function(){
     /** Click event on Toggle Menu */
     $toggleCollapse.click(function(){
         $nav.toggleClass('collapse');
-    })
+    });
+
+    // Owl Carousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+    });
 
 });
