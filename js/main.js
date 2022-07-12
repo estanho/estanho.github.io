d6664c7ee86008da08931bbfe133693d2ae7cd66
Pedro Henrique da Rosa Garcia <estanhoeu@gmail.com>
2022-07-12 19:11:02 -0300

    Mais atualizações 😅

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
 $(document).ready(function(){
 
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapse');
 
-    /** Click event on Toggle Menu */
+    //Click event on Toggle Menu
     $toggleCollapse.click(function(){
         $nav.toggleClass('collapse');
     });
@@ -15,7 +27,18 @@ $(document).ready(function(){
         autoplayTimeout: 10000,
         dots: false,
         nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
     });
+
+    // Click to scroll top
+    $('.move-up span').click(function(){
+        $('html,body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+    //AOS Instance
+    AOS.init();
 
 });
