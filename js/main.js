8b0f9d82304486ff9d263939eee74e0d36e28f7b
Pedro Henrique da Rosa Garcia <estanhoeu@gmail.com>
2022-07-18 20:29:27 -0300

    Refeito carregamento do carousel 🎡

--- js/main.js
+++ js/main.js
@@ -12,8 +12,16 @@ const responsive = {
 
 $(document).ready(function(){
 
-    $.get( "./posts.json", function(data) {
-        data.posts.forEach(function(post){
+    var posts;
+
+    // Load json
+    $.getJSON( "../posts.json", (data) => {
+        posts = data.posts;
+    });
+
+    // Carousel
+    $.when().then(() => {
+        posts.forEach((post) => {
             $(".owl-carousel").append(
                 `<div class="blog-content" data-aos="fade-right" data-aos-delay="200">
                     <img src="${post.image}" alt="post-1">
@@ -23,20 +31,16 @@ $(document).ready(function(){
                         <span>${post.day}</span>
                     </div>
                 </div>`);
-                console.log('Foi');
-        });
-    });
+            });
+    })
+    .done(() => {
+        $(".blog .container").append(`
+            <div class="owl-navigation">
+            <span class="owl-nav-prev"><i class="fas fa-long-arrow-alt-left"></i></span>
+            <span class="owl-nav-next"><i class="fas fa-long-arrow-alt-right"></i></span>
+            </div>`
+        );
 
-    $nav = $('.nav');
-    $toggleCollapse = $('.toggle-collapse');
-
-    //Click event on Toggle Menu
-    $toggleCollapse.click(function(){
-        $nav.toggleClass('collapse');
-    });
-
-    setTimeout(function(){
-        // Owl Carousel for blog
         $('.owl-carousel').owlCarousel({
             loop: true,
             autoplay: true,
@@ -46,8 +50,47 @@ $(document).ready(function(){
             navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
             responsive: responsive
         });
-     }, 300);
+    });
+    
+    $(window).scroll(function() {
+        if($(window).scrollTop() == $(document).height() - $(window).height()) {
+            $(".posts").append(
+                `<hr>
+                <div class="post-content" data-aos="zoom-in" data-aos-delay="200">
+                    <div class="post-image">
+                        <div>
+                            <img src="./assets/images_posts/background_test.jpg" class="img" alt="blog1">
+                        </div>
+                        <div class="post-info flex-row">
+                            <span><i class="fas fa-user text-gray"></i>&nbsp;&nbsp;Admin</span>
+                            <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;Janeiro 14, 2022</span>
+                            <span>2 Comentários</span>
+                        </div>
+                    </div>
+                    <div class="post-title">
+                        <a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, hic?</a>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, 
+                            repellendus molestias esse officia pariatur libero, illo sunt iure 
+                            accusantium aliquid dicta eum porro consequuntur dolorem non eos! 
+                            Quam commodi molestias quae repellat alias aliquam reprehenderit 
+                            nesciunt esse dolores eum! Illum!
+                        </p>
+                        <button class="btn post-btn">Read More &nbsp;<i class="fas fa-arrow-right"></i></button>
+                    </div>
+                </div>`);
+        }
+    });
+    
 
+    // Mobile
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    //Click event on Toggle Menu
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+    });
+    
     // Click to scroll top
     $('.move-up span').click(function(){
         $('html,body').animate({
